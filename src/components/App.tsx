c03d3f7e06d832128dcb0e8f8c606fdfd4f00d21
Friedrich Schöne <friedrich.schoene@gmx.de>
2021-01-07 11:40:16 +0100

    solarized editor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import Editor from '@monaco-editor/react';
+import { ControlledEditor, monaco } from '@monaco-editor/react';
+import { editor } from 'monaco-editor/esm/vs/editor/editor.api';
+
+import solarized from 'monaco-themes/themes/Solarized-light.json';
+
+monaco
+  .init()
+  .then((monacoInstance) => monacoInstance.editor.defineTheme('solarized', solarized as editor.IStandaloneThemeData));
 
 const App = (): React.ReactElement => (
   <>
-    <Editor
+    <ControlledEditor
       height="100vh"
       language="markdown"
       options={{
-        // hides line numbers
-        lineNumbers: 'off',
-        // hide folding option
         folding: false,
-        // automatical line break
         wordWrap: 'on',
       }}
+      theme="solarized"
     />
   </>
 );
